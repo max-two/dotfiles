5a603c150cf9e08b974c788427c628a2655dc018
Max McKinley <max.mckinley@dover.com>
2022-11-07 18:51:29 -0600

    Fixing path for bins

--- private_dot_config/private_fish/conf.d/env.fish
+++ private_dot_config/private_fish/conf.d/env.fish
@@ -1,3 +1,3 @@
-set --export --prepend PATH /usr/local/opt/ruby/bin /usr/local/lib/ruby/gems/3.1.0/bin
+set --export --prepend PATH /usr/local/bin /usr/local/opt/ruby/bin /usr/local/lib/ruby/gems/3.1.0/bin
 set --export EDITOR hx
 set --export CODE $HOME/code
